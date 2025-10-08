f815c9da97c756ba1c2d9a0947dd9bcc718a7825
vladg44 <gogavlad23@stud.ase.ro>
2025-10-08 09:46:27 +0000

    server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-var http = require("http");
+const express = require("express");
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(8080);
